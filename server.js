eb5eae61985dac42719a85a593aefebc06b24d72
Felicia Wootton <felicia.wootton@gmail.com>
2021-04-25 11:04:23 -0500

    deleted unneccessary functions

--- server.js
+++ server.js
@@ -137,44 +137,6 @@ const viewByDept = () => {
 };
 
 
-const viewLegalDept = () => {
-    connection.query(`SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name 
-    AS department, role.salary, 
-    CONCAT(manager.first_name, " ", manager.last_name) 
-    AS manager
-    FROM employee 
-    LEFT JOIN employee manager on manager.id = employee.manager_id
-    INNER JOIN role 
-    ON (role.id = employee.role_id) 
-    INNER JOIN department 
-    ON (department.id = role.department_id)
-    ORDER BY department`, (err, results) => {
-        if (err) throw err;
-        console.table(results);
-        continueOrExit();
-    })
-};
-
-
-const viewSalesDept = () => {
-    connection.query(`SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name 
-    AS department, role.salary, 
-    CONCAT(manager.first_name, " ", manager.last_name) 
-    AS manager
-    FROM employee 
-    LEFT JOIN employee manager on manager.id = employee.manager_id
-    INNER JOIN role 
-    ON (role.id = employee.role_id) 
-    INNER JOIN department 
-    ON (department.id = role.department_id)
-    ORDER BY department`, (err, results) => {
-        if (err) throw err;
-        console.table(results);
-        continueOrExit();
-    })
-};
-
-
 const viewByManager = () => {
     connection.query(`SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name 
     AS department, role.salary, 
