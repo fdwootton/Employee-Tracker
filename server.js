bf90ca61bd2e94cae2ae677e25ad2cd46162a8a4
Felicia Wootton <felicia.wootton@gmail.com>
2021-04-25 18:47:23 -0500

    edited view all employees function

--- server.js
+++ server.js
@@ -51,13 +51,15 @@ const start = () => {
 
 
 const viewAllEmployees = () => {
-    connection.query(`SELECT employee.id, employee.first_name, employee.last_name, role.title, 
+    connection.query(`SELECT employee.id, 
+    CONCAT(employee.first_name, " ", employee.last_name) AS name, 
+    role.title, 
     department.name AS department, 
     role.salary, 
-    CONCAT(manager.first_name, " ", manager.last_name) AS manager
+    CONCAT(newTable.first_name, " ", newTable.last_name) AS manager
     FROM employee 
-    LEFT JOIN employee manager 
-    ON manager.id = employee.manager_id
+    LEFT JOIN employee AS newTable 
+    ON newTable.id = employee.manager_id
     INNER JOIN role 
     ON role.id = employee.role_id 
     INNER JOIN department 
