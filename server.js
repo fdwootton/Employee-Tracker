52c839d2e96b6bf3542f0f30fb861a29627ae22c
Felicia Wootton <felicia.wootton@gmail.com>
2021-04-24 23:27:18 -0500

    Added blueprint for read functions

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
-// require('console.table');
 const cTable = require('console.table');
 
 
@@ -27,7 +26,11 @@ const start = () => {
         message: 'What would you like to do?',
         choices: ['View All Employees', 
         'View All Employees By Department', 
-        'View All Employees By Manager', 
+        'View All Employees By Manager',
+        'Add Employee',
+        'Add Role',
+        'Add Department', 
+        'Update Employee Role',
         'Exit'],
       })
       .then((answer) => {
@@ -49,7 +52,17 @@ const start = () => {
 
 
 const viewAllEmployees = () => {
-    const query = connection.query('SELECT * FROM employee_db.employee', (err, results) => {
+    connection.query(`SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name 
+    AS department, role.salary, 
+    CONCAT(manager.first_name, " ", manager.last_name) 
+    AS manager
+    FROM employee 
+    LEFT JOIN employee manager on manager.id = employee.manager_id
+    INNER JOIN role 
+    ON (role.id = employee.role_id) 
+    INNER JOIN department 
+    ON (department.id = role.department_id)
+    ORDER BY employee.id`, (err, results) => {
         if (err) throw err;
         console.table(results);
         continueOrExit();
@@ -75,6 +88,129 @@ const continueOrExit = () => {
           connection.end();
         }
       });
+};
+
+
+const viewByDept = () => {
+    inquirer
+    .prompt({
+      name: 'chooseDept',
+      type: 'list',
+      message: 'Choose a department:',
+      choices: ['Engineering', 
+      'Finance', 
+      'Legal', 
+      'Sales']
+    })
+    .then((answer) => {
+      if (answer.chooseDept === 'Engineering') {
+        viewEngineerDept();
+      } else if (answer.chooseAction === 'Finance') {
+        viewFinanceDept();
+      } else if (answer.chooseAction === 'Legal'){
+        viewLegalDept();
+      } else if (answer.chooseAction === 'Sales'){
+        viewSalesDept();
+      }
+      else {
+        connection.end();
+      }
+    });
+};
+
+
+const viewEngineerDept = () => {
+    connection.query(`SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name 
+    AS department, role.salary, 
+    CONCAT(manager.first_name, " ", manager.last_name) 
+    AS manager
+    FROM employee 
+    LEFT JOIN employee manager on manager.id = employee.manager_id
+    INNER JOIN role 
+    ON (role.id = employee.role_id) 
+    INNER JOIN department 
+    ON (department.id = role.department_id)
+    ORDER BY department`, (err, results) => {
+        if (err) throw err;
+        console.table(results);
+        continueOrExit();
+    })
+};
+
+
+const viewFinanceDept = () => {
+    connection.query(`SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name 
+    AS department, role.salary, 
+    CONCAT(manager.first_name, " ", manager.last_name) 
+    AS manager
+    FROM employee 
+    LEFT JOIN employee manager on manager.id = employee.manager_id
+    INNER JOIN role 
+    ON (role.id = employee.role_id) 
+    INNER JOIN department 
+    ON (department.id = role.department_id)
+    ORDER BY department`, (err, results) => {
+        if (err) throw err;
+        console.table(results);
+        continueOrExit();
+    })
+};
+
+
+const viewLegalDept = () => {
+    connection.query(`SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name 
+    AS department, role.salary, 
+    CONCAT(manager.first_name, " ", manager.last_name) 
+    AS manager
+    FROM employee 
+    LEFT JOIN employee manager on manager.id = employee.manager_id
+    INNER JOIN role 
+    ON (role.id = employee.role_id) 
+    INNER JOIN department 
+    ON (department.id = role.department_id)
+    ORDER BY department`, (err, results) => {
+        if (err) throw err;
+        console.table(results);
+        continueOrExit();
+    })
+};
+
+
+const viewSalesDept = () => {
+    connection.query(`SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name 
+    AS department, role.salary, 
+    CONCAT(manager.first_name, " ", manager.last_name) 
+    AS manager
+    FROM employee 
+    LEFT JOIN employee manager on manager.id = employee.manager_id
+    INNER JOIN role 
+    ON (role.id = employee.role_id) 
+    INNER JOIN department 
+    ON (department.id = role.department_id)
+    ORDER BY department`, (err, results) => {
+        if (err) throw err;
+        console.table(results);
+        continueOrExit();
+    })
+};
+
+
+const viewByManager = () => {
+    connection.query(`SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name 
+    AS department, role.salary, 
+    CONCAT(manager.first_name, " ", manager.last_name) 
+    AS manager
+    FROM employee 
+    LEFT JOIN employee manager on manager.id = employee.manager_id
+    INNER JOIN role 
+    ON (role.id = employee.role_id) 
+    INNER JOIN department 
+    ON (department.id = role.department_id)
+    ORDER BY manager.id`, (err, results) => {
+        if (err) throw err;
+        console.table(results);
+        continueOrExit();
+    })
 };
 
 
